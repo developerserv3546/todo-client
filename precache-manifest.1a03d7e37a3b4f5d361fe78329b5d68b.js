a5ae0ccf97724590f907beafaa8868e98d394126
Sergey <sergey@macbook.local>
2020-06-23 12:16:10 -0700

    Updates

---
+++ precache-manifest.1a03d7e37a3b4f5d361fe78329b5d68b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f95b6ad32f9d9333c10f0f388fa37b",
+    "url": "/todo-client/index.html"
+  },
+  {
+    "revision": "9043fe6c3ec978b925a9",
+    "url": "/todo-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "42d1be838a51011951f7",
+    "url": "/todo-client/static/css/main.e8e9a996.chunk.css"
+  },
+  {
+    "revision": "9043fe6c3ec978b925a9",
+    "url": "/todo-client/static/js/2.9a7efa62.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-client/static/js/2.9a7efa62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d1be838a51011951f7",
+    "url": "/todo-client/static/js/main.a31583c1.chunk.js"
+  },
+  {
+    "revision": "2351702386764fb53840",
+    "url": "/todo-client/static/js/runtime-main.1f8bd8dd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/todo-client/precache-manifest.1a03d7e37a3b4f5d361fe78329b5d68b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
